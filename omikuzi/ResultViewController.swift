fc1f5d331520826a5a3b859d7755f9bdb1e7f6fb
Kahototo <johndoe@example.k.t.07242@gmail.com>
2016-07-11 09:15:49 +0900

    omikuzi

--- omikuzi/ResultViewController.swift
+++ omikuzi/ResultViewController.swift
@@ -10,28 +10,38 @@ import UIKit
 
 class ResultViewController: UIViewController {
     
+    
     @IBOutlet var label: UILabel!
     
     var omikuziArray: [UILabel]!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    // Do any additional setup after loading the view, typicallyfrom a nib.
-    {
-    
-    @IBAction func plusButton(){
         
-        label.text = String(label)
+        let number = Int(rand() % 9)
         
-        if label = "大吉" {
+        omikuziArray = [UIImage(named: "大吉")!,
+                        UILabel(named: "凶")!,
+                        UILabel(named: "吉")!,
+                        UILabel(named: "吉")!,
+                        UILabel(named: "吉")!,
+                        UILabel(named: "中吉")!,
+                        UILabel(named: "中吉")!,
+                        UILabel(named: "小吉")!,
+                        UILabel(named: "小吉")!,
+                        UILabel(named: "小吉")!
+            
+        ]
+        
+        label.text = omikuziArray[number]
+        
+        if label.text == "大吉" {
             label.textColor = UIColor.redColor()
         } else {
             label.textColor = UIColor.yellowColor()
         }
-        override func didReceiveMemoryWarning() {
+        
+        func didReceiveMemoryWarning() {
             super.didReceiveMemoryWarning()
             // Dispose of any resources that can be recreated.
         }
@@ -50,12 +60,11 @@ class ResultViewController: UIViewController {
 /*
  // MARK: - Navigation
  
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
-}
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+ // Get the new view controller using segue.destinationViewController.
+ // Pass the selected object to the new view controller.
+ }
+ */
+
 
